0ab33745d9b21feff98148a4c7e6047eb562e8f5
MauricioOliveira-Git <mauricioliveirafilho10@gmail.com>
2025-10-30 22:40:42 -0300

    Create folder e file Dto

---
+++ src/main/java/com/deliverytech/delivery/dto/OrderItemDTO.java
@@ -0,0 +1,13 @@
+
+package com.deliverytech.delivery.dto;
+
+public class OrderItemDTO {
+    private Long productId;
+    private Integer quantity;
+
+    public Long getProductId() { return productId; }
+    public void setProductId(Long productId) { this.productId = productId; }
+    
+    public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+}
